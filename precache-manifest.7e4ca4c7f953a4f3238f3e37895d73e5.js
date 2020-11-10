4d54e29f3738b9b3151e1c718e8703258f0ee092
Plamen Pentchev <plamenpentchev@yahoo.com>
2020-11-10 18:07:57 +0100

    Updates

---
+++ precache-manifest.7e4ca4c7f953a4f3238f3e37895d73e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62dcf9661c5b01c0acda34f260721021",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db8a214fdfc24cbf356c",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "bd6bb16ad706fa90940f",
+    "url": "./static/js/2.79227bc3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.79227bc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db8a214fdfc24cbf356c",
+    "url": "./static/js/main.27a4ec21.chunk.js"
+  },
+  {
+    "revision": "05dfe8b9d61a96d76ed1",
+    "url": "./static/js/runtime-main.0e7e22de.js"
+  }
+]);
